acaa914a2a2058d93b137368fd36e572b3bdf383
AlbaUka <albaukaihm@gmail.com>
2023-12-12 20:07:49 +0100

    new

---
+++ index.js
@@ -0,0 +1,74 @@
+// Objects
+const article = document.querySelectorAll("div");
+const button = document.querySelectorAll(`input[type="button"]`);
+const img = document.querySelectorAll("img");
+
+const austronaut = {
+  name: "Austronaut",
+  costs: "100kr",
+  article: article[0],
+  button: button[0],
+  img: img[0],
+};
+
+const ninja = {
+  name: "Ninja",
+  costs: "120kr",
+  article: article[1],
+  button: button[1],
+  img: img[1],
+};
+
+const nerd = {
+  name: "Nerd",
+  costs: "210kr",
+  article: article[2],
+  button: button[2],
+  img: img[2],
+};
+
+const doctor = {
+  name: "Doctor",
+  costs: "120kr",
+  article: article[3],
+  button: button[3],
+  img: img[3],
+};
+
+console.log(austronaut, ninja, nerd, doctor);
+
+// const guitarPlayer = {
+//     article:
+//     button:
+//     img:
+// }
+// const snorkleMan = {
+//     article:
+//     button:
+//     img:
+// }
+// const clown = {
+//     article:
+//     button:
+//     img:
+// }
+// const policeOfficer = {
+//     article:
+//     button:
+//     img:
+// }
+// const fairy = {
+//     article:
+//     button:
+//     img:
+// }
+// const photographer = {
+//     article:
+//     button:
+//     img:
+// }
+// const personalTrainer = {
+//     article:
+//     button:
+//     img:
+// }

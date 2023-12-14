c084c542541bb4d5467c8893c2de381003cee320
AlbaUka <albaukaihm@gmail.com>
2023-12-14 11:24:48 +0100

    Did cart + trying on javascript

--- index.js
+++ index.js
@@ -1,74 +1,15 @@
-// Objects
-const article = document.querySelectorAll("div");
-const button = document.querySelectorAll(`input[type="button"]`);
-const img = document.querySelectorAll("img");
+let iconCart = document.querySelector(".icon-cart");
+let body = document.querySelector("body");
+let listProductHTML = document.querySelector(".listProduct");
 
-const austronaut = {
-  name: "Austronaut",
-  costs: "100kr",
-  article: article[0],
-  button: button[0],
-  img: img[0],
+let listProducts = [];
+
+const initApp = () => {
+  fetch("products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      listProducts = data;
+    });
+  console.log(listProducts);
 };
-
-const ninja = {
-  name: "Ninja",
-  costs: "120kr",
-  article: article[1],
-  button: button[1],
-  img: img[1],
-};
-
-const nerd = {
-  name: "Nerd",
-  costs: "210kr",
-  article: article[2],
-  button: button[2],
-  img: img[2],
-};
-
-const doctor = {
-  name: "Doctor",
-  costs: "120kr",
-  article: article[3],
-  button: button[3],
-  img: img[3],
-};
-
-console.log(austronaut, ninja, nerd, doctor);
-
-// const guitarPlayer = {
-//     article:
-//     button:
-//     img:
-// }
-// const snorkleMan = {
-//     article:
-//     button:
-//     img:
-// }
-// const clown = {
-//     article:
-//     button:
-//     img:
-// }
-// const policeOfficer = {
-//     article:
-//     button:
-//     img:
-// }
-// const fairy = {
-//     article:
-//     button:
-//     img:
-// }
-// const photographer = {
-//     article:
-//     button:
-//     img:
-// }
-// const personalTrainer = {
-//     article:
-//     button:
-//     img:
-// }
+console.log(listProducts);
